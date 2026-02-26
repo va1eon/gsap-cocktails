64453304c1b8f4acfbe2bfde06184cf82a0f31bd
Valery Mikhailov <va1eonmix@gmail.com>
2026-02-26 16:10:53 +0700

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 
+import Navbar from './components/Navbar'
+
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
 	return (
-		<div className="flex-center h-screen">
-			<h1 className="font-modern-negra text-3xl text-indigo-300">
-				Hello World!
-			</h1>
-		</div>
+		<main>
+			<Navbar />
+		</main>
 	)
 }
 export default App
---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const navLinks = [
+	{ id: 'cocktails', title: 'Cocktails' },
+	{ id: 'about', title: 'About Us' },
+	{ id: 'work', title: 'The Art' },
+	{ id: 'contact', title: 'Contact' },
+]
+
+export { navLinks }
