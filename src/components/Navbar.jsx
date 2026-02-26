80325f9a06d84baf23be8be8aa93cbd109e83cfc
Valery Mikhailov <va1eonmix@gmail.com>
2026-02-26 16:55:52 +0700

    fix backdropFilter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
 			},
 			{
 				backgroundColor: '#00000050',
-				backgroundFilter: 'blur(10px)',
+				backdropFilter: 'blur(10px)',
 				duration: 1,
 				ease: 'power1.inOut',
 			}
 		)
-	})
+	}, [])
 
 	return (
 		<nav>
