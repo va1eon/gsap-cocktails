70cdd3df2400340e0f1d593a399d41e962ca7b25
Valery Mikhailov <va1eonmix@gmail.com>
2026-02-27 13:39:15 +0700

    add Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About'
 import Art from './components/Art'
 import Cocktails from './components/Cocktails'
 import Hero from './components/Hero'
+import Menu from './components/Menu'
 import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
@@ -17,6 +18,7 @@ const App = () => {
 			<Cocktails />
 			<About />
 			<Art />
+			<Menu />
 		</main>
 	)
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -73,4 +73,46 @@ const featureLists = [
 	'Expertly shaken & stirred',
 ]
 
-export { navLinks, cocktailLists, mockTailLists, goodLists, featureLists }
+const allCocktails = [
+	{
+		id: 1,
+		name: 'Classic Mojito',
+		image: './images/drink1.png',
+		title: 'Simple Ingredients, Bold Flavor',
+		description:
+			'Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.',
+	},
+	{
+		id: 2,
+		name: 'Raspberry Mojito',
+		image: './images/drink2.png',
+		title: 'A Zesty Classic That Never Fails',
+		description:
+			'The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.',
+	},
+	{
+		id: 3,
+		name: 'Violet Breeze',
+		image: './images/drink3.png',
+		title: 'Simple Ingredients, Bold Flavor',
+		description:
+			'Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.',
+	},
+	{
+		id: 4,
+		name: 'Curacao Mojito',
+		image: './images/drink4.png',
+		title: 'Crafted With Care, Poured With Love',
+		description:
+			"Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
+	},
+]
+
+export {
+	navLinks,
+	cocktailLists,
+	mockTailLists,
+	goodLists,
+	featureLists,
+	allCocktails,
+}
